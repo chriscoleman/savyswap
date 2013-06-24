4303eec79890308b396f7e6a8ca66f7e26e1ac92
Chris Coleman <ccoleman2k9@gmail.com>
2013-06-24 02:01:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savyswap::Application.config.secret_token = 'a6519f730c4dfb6d469198fc0c4d0db4f8653211e6b504a707908f4130b931e8f1662db417824bb5642b95f6d5fe01d3e17467a73706e90e5a4fbf921d455688'
