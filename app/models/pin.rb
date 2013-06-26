7ceaf490b71fe394893422da21744f1e10c4a014
Chris Coleman <ccoleman2k9@gmail.com>
2013-06-25 21:23:18 -0400

    Add Pins Association

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
   attr_accessible :
 
-  validates :description, presence: true, 
+  validates :description, presence: true
+
+  belongs_to :user
+	validates :user_id, presence: true
+
 end
